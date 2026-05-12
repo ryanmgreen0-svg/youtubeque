8113b1218a3f614e1ed2121e272331f9d6be8fa7
Lasingu7753 <ryanmgreen96@gmail.com>
2026-05-12 14:29:45 -0700

    xx

--- queue.js
+++ queue.js
@@ -11,22 +11,13 @@ function displayQueue() {
     }
     queue.forEach((video, index) => {
       let div = document.createElement('div');
-      div.style.margin = '10px';
-      div.style.border = '1px solid #ccc';
-      div.style.padding = '10px';
+      div.className = 'video-card';
       div.innerHTML = `
-        <img src="${video.thumbnail}" width="120" style="float:left; margin-right:10px;">
+        <img src="${video.thumbnail}" alt="${video.title}">
         <h3>${video.title}</h3>
-        <button class="open-remove-button" data-index="${index}">Open and Remove</button>
       `;
+      div.addEventListener('click', () => openVideo(index));
       container.appendChild(div);
-    });
-
-    container.querySelectorAll('.open-remove-button').forEach(button => {
-      button.addEventListener('click', event => {
-        let index = Number(event.currentTarget.getAttribute('data-index'));
-        openVideo(index);
-      });
     });
   });
 }
