b443769d56c7dc81d8e87b1cc1c65b4f139e05db
Lasingu7753 <ryanmgreen96@gmail.com>
2026-05-12 12:25:14 -0700

    dd

--- background.js
+++ background.js
@@ -1,20 +1,14 @@
 function createQueueMenu() {
   chrome.contextMenus.removeAll(() => {
     chrome.contextMenus.create({
+      id: "add-to-queue",
       title: "Add to Queue",
-      contexts: ["page", "video", "image", "link"],
-      documentUrlPatterns: ["*://www.youtube.com/*"],
-      onclick: function(info, tab) {
-        chrome.tabs.sendMessage(tab.id, {action: "queueVideo", info: info}, function(response) {
-          if (response && response.video) {
-            chrome.storage.local.get(['queue'], function(result) {
-              let queue = result.queue || [];
-              queue.push(response.video);
-              chrome.storage.local.set({queue: queue});
-            });
-          }
-        });
-      }
+      contexts: ["all"],
+      documentUrlPatterns: [
+        "*://www.youtube.com/*",
+        "*://youtube.com/*",
+        "*://youtu.be/*"
+      ]
     });
   });
 }
@@ -22,3 +16,19 @@ function createQueueMenu() {
 chrome.runtime.onInstalled.addListener(createQueueMenu);
 chrome.runtime.onStartup.addListener(createQueueMenu);
 createQueueMenu();
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId !== "add-to-queue") {
+    return;
+  }
+
+  chrome.tabs.sendMessage(tab.id, {action: "queueVideo", info: info}, function(response) {
+    if (response && response.video) {
+      chrome.storage.local.get(['queue'], function(result) {
+        let queue = result.queue || [];
+        queue.push(response.video);
+        chrome.storage.local.set({queue: queue});
+      });
+    }
+  });
+});
